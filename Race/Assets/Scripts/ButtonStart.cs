5ad41888243262a16b8c39afaae74248518b2ef1
Karifannn <mr.prishencko@gmail.com>
2018-02-22 17:53:23 +0300

    ро

---
+++ Race/Assets/Scripts/ButtonStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStart : MonoBehaviour {
+
+    private void OnMouseUpAsButton()
+    {
+        switch (gameObject.name)
+        {
+            case "b_Start":
+                Application.LoadLevel("menu");
+                break;
+
+            case "play_Easy":
+                Application.LoadLevel("gameEasy3x3");
+                break;
+
+        }
+    }
+}
---
+++ Race/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour {
+
+    public GameObject obj;
+    public float range = 2f;
+   // private float stop;
+
+    private void OnMouseUpAsButton()
+    {
+      
+        
+            if (gameObject.name == "ClickRight" && obj.transform.position.x < 2)
+            {
+                obj.transform.Translate(Vector3.right * range);
+             //   stop = stop + range;
+            }
+
+            if (gameObject.name == "ClickLeft" && obj.transform.position.x > -2)
+            {
+                obj.transform.Translate(-Vector3.right * range);
+              //  stop = stop - range;
+            }
+
+    }
+
+}
