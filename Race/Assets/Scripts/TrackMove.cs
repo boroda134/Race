e8e4554f2c3feacf13c90075e66092f68cb03def
Karifannn <mr.prishencko@gmail.com>
2018-02-22 21:46:50 +0300

    script

---
+++ Race/Assets/Scripts/TrackMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackMove : MonoBehaviour {
+
+    public float speed;
+    Vector2 offset;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+		
+	}
+}
