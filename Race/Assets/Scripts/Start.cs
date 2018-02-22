7921473859037807eab9fb4e5169f45e71eb25df
Karifannn <mr.prishencko@gmail.com>
2018-02-22 20:59:52 +0300

    button

---
+++ Race/Assets/Scripts/Start.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour {
+
+    public void StartToScene (string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+    
+        
+    }
+
+
